cc438d5250abf4ec7579d91b012efe9c63f17a7d
Maisie <maisieobb@googlemail.com>
2021-03-14 23:18:51 +0000

    Location form added but not working

--- client/workwithme/src/App.js
+++ client/workwithme/src/App.js
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import { MapContainer, Map, Marker, Popup, TileLayer } from "react-leaflet";
-// // import { Icon } from "leaflet";
+import CityForm from "./Components/CityForm";
 
-const position = [51.505, -0.09];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Here's our map! It's highly editable by the way :)
-        
-        <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>Buuble 1</Popup>
-          </Marker>
-        </MapContainer>
-        
+        <CityForm />
       </header>
     </div>
   );
