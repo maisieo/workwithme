bb5d524bdde444c662d0e130842b0c80bb6c8189
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-03-18 15:08:36 -0400

    "Updated data"

--- users.sql
+++ users.sql
@@ -1,2 +1,2 @@
 INSERT INTO `workwithme` VALUES 
-    (1,'rhys','$2b$12$eFzMWbS9SogNtxkmo3J7aO8FQMFQSKbtpwLMIOVsF6GGKpTQdgq.W','thisisademo@email.com');
+    (1,'rhys','$2y$12$YPj.pOOy1zCDM1ybmbZK1elaFe81hNfWfiKbcAejXF3K2vH479KCW','thisisademo@email.com');
