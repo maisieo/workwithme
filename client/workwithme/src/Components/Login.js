e21f4ccb3755374c4ee5f61a5d078a1577a4a854
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-03-24 21:41:39 -0400

    "Refactored code"

--- client/workwithme/src/Components/Login.js
+++ client/workwithme/src/Components/Login.js
@@ -22,12 +22,18 @@ function Login(props) {
 
     function doLogin(username,password) {
         // login
-        console.log(username, password)
+        // console.log(username, password)
+    let user = {username, password}
+    fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .catch((err) => console.log("ERROR:", err.message));
     }
-
-    // function onSubmit(username, password) {
-        
-    // }
 
     function handleSubmit(event) {
         event.preventDefault();
