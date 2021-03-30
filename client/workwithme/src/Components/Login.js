074cc35b522e6b027d91863eff1e66fa26b663d5
Maisie <maisieobb@googlemail.com>
2021-03-30 15:47:18 +0100

    Couple more CSS changes

--- client/workwithme/src/Components/Login.js
+++ client/workwithme/src/Components/Login.js
@@ -1,45 +1,44 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    function handleChange(event) {
-        let { name, value } = event.target;
-        switch (name) {
-            case 'usernameInput':
-                setUsername(value);
-                break;
-            case 'passwordInput':
-                setPassword(value);
-                break;
-            default:
-                break;
-        }
+  function handleChange(event) {
+    let { name, value } = event.target;
+    switch (name) {
+      case "usernameInput":
+        setUsername(value);
+        break;
+      case "passwordInput":
+        setPassword(value);
+        break;
+      default:
+        break;
     }
+  }
 
-    function doLogin(username,password) {
-        // login
-        console.log(username, password)
-    }
+  function doLogin(username, password) {
+    // login
+    console.log(username, password);
+  }
 
-    // function onSubmit(username, password) {
-        
-    // }
+  // function onSubmit(username, password) {
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        doLogin(username, password);
-    }
+  // }
 
-    return (
-        // <div className="Login">
-        //     <div className="col-4 offset-4">
-        //         <h2>Login</h2>
-                
-        //         {
+  function handleSubmit(event) {
+    event.preventDefault();
+    doLogin(username, password);
+  }
+
+  return (
+    <div className="Login">
+      {/* //     <div className="col-4 offset-4"> */}
+      <h2>Login</h2>
+
+      {/* //         {
         //             props.error && (
         //                 <div>{props.error}</div>
         //             )
@@ -74,27 +73,38 @@ function Login(props) {
         //         </form>
         //         <Link to="/register">Don't have an account yet? Register now!</Link>
         //     </div>
-        // </div>
-        <div>
+        // </div> */}
+      <div>
         <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-    );
-
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            ></input>
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            ></input>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- client/workwithme/src/Components/Register.js
+++ client/workwithme/src/Components/Register.js
@@ -1,45 +1,121 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function Register(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    function handleChange(event) {
-        let { name, value, email } = event.target;
-        switch (name) {
-            case 'emailInput':
-                setEmail(value);
-                break;
-            case 'usernameInput':
-                setUsername(value);
-                break;
-            case 'passwordInput':
-                setPassword(value);
-                break;
-            default:
-                break;
-        }
+  function handleChange(event) {
+    let { name, value, email } = event.target;
+    switch (name) {
+      case "emailInput":
+        setEmail(value);
+        break;
+      case "usernameInput":
+        setUsername(value);
+        break;
+      case "passwordInput":
+        setPassword(value);
+        break;
+      default:
+        break;
     }
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        props.onSubmit(email, username, password);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onSubmit(email, username, password);
+  }
 
-    return (
-        <div className="Register">
-            <div className="col-4 offset-4">
-                <h2>Register</h2>
-                
-                {
-                    props.error && (
-                        <div>{props.error}</div>
-                    )
-                }
+  return (
+    <div className="Register">
+      <div className="col-4 offset-4">
+        <h2>Register</h2>
 
-                <form onSubmit={handleSubmit}>
+        {props.error && <div>{props.error}</div>}
+
+        {
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  required
+                  value={username}
+                  onChange={handleChange}
+                ></input>
+                <small id="emailHelp" class="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                  required
+                  value={password}
+                  onChange={handleChange}
+                ></input>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Username</label>
+                <input
+                  type="username"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="username"
+                  required
+                  value={username}
+                  onChange={handleChange}
+                ></input>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+
+          //   <Form onSubmit={handleSubmit}>
+          //     <Form.Group controlId="exampleForm.ControlInput1">
+          //       <Form.Label>Username</Form.Label>
+          //       <Form.Control
+          //         type="username"
+          //         placeholder="example123"
+          //         required
+          //         value={username}
+          //         onChange={handleChange}
+          //       />
+          //     </Form.Group>
+          //     <Form.Group controlId="exampleForm.ControlInput1">
+          //       <Form.Label>Email address</Form.Label>
+          //       <Form.Control
+          //         type="email"
+          //         placeholder="name@example.com"
+          //         required
+          //         value={email}
+          //         onChange={handleChange}
+          //       />
+          //     </Form.Group>
+          //     <Form.Group controlId="exampleForm.ControlInput1">
+          //       <Form.Label>Password</Form.Label>
+          //       <Form.Control
+          //         type="password"
+          //         placeholder="PasswOrd12"
+          //         required
+          //         value={password}
+          //         onChange={handleChange}
+          //       />
+          //     </Form.Group>
+          //   </Form>
+          /* <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label>Username
                             <input
@@ -76,11 +152,11 @@ function Register(props) {
                         </label>
                     </div>
                     <button type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
-    );
-
+                </form> */
+        }
+      </div>
+    </div>
+  );
 }
 
 export default Register;
