7b8feea47688cf84ca6c5af60d1e3692710b90f7
Maisie <maisieobb@googlemail.com>
2021-08-03 20:00:23 +0100

    index router change

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const { ensureUserLoggedIn } = require('../middleware/guards');
 
 // home page
-router.get('/', function(req, res, next) {
-  res.send({ message: 'Welcome to Work With Me! Where would you like to go?' });
-});
+// router.get('/', function(req, res, next) {
+//   res.send({ message: 'Welcome to Work With Me! Where would you like to go?' });
+// });
 
 // // members only 
 // router.get('/members-only', ensureUserLoggedIn, function(req, res, next) {
