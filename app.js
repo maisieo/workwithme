2f5a28786437673f24145bec8cbe5c9a9532d729
Maisie <maisieobb@googlemail.com>
2021-08-08 14:34:25 +0100

    change to index router

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 // var authRouter = require("./routes/auth");
-var indexRouter = require("./routes/bubbles");
+var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
 var bubblesRouter = require("./routes/bubbles");
 
